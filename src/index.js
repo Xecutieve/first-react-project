8ba235c6263339a753f7455eb32462b58600d716
Görkem Cindemir <pgork@iMac.local>
2025-04-10 11:40:26 +0300

    first-react-project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,98 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <div>
+      <img class="avatar" src="/profile.jpeg" alt="profile" />
+    </div>
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Görkem Cindemir</h1>
+      <p>Learning Full-Stack React Development For My Career</p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div>
+      <ul class="skill-list">
+        <Skill skill="HTML" style={{ backgroundColor: "blue" }} />
+        <Skill skill="CSS" style={{ backgroundColor: "red" }} />
+        <Skill skill="JavaScript" style={{ backgroundColor: "yellow" }} />
+        <Skill skill="React" style={{ backgroundColor: "lightBlue" }} />
+        <Skill
+          skill="Git, Github and Gitlab"
+          style={{ backgroundColor: "orange" }}
+        />
+        <Skill skill="PostgreSQL" style={{ backgroundColor: "gray" }} />
+        <Skill skill="BackEnd" style={{ backgroundColor: "green" }} />
+        <Skill skill="Postman" style={{ backgroundColor: "khaki" }} />
+      </ul>
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <li class="skill" style={props.style}>
+      {props.skill}
+    </li>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/*
+<li class="skill" style={{ backgroundColor: "red" }}>
+        HTML + CSS
+      </li>
+      <li class="skill" style={{ backgroundColor: "yellow" }}>
+        JavaScript
+      </li>
+      <li class="skill" style={{ backgroundColor: "blue" }}>
+        React
+      </li>
+      <li class="skill" style={{ backgroundColor: "gray" }}>
+        SQL
+      </li>
+      <li class="skill" style={{ backgroundColor: "lightGray" }}>
+        PostgreSQL
+      </li>
+      <li class="skill" style={{ backgroundColor: "lightBlue" }}>
+        Next.js{" "}
+      </li>
+      <li class="skill" style={{ backgroundColor: "orange" }}>
+        Git Github Gitlab
+      </li>
+      <li class="skill" style={{ backgroundColor: "green" }}>
+        Back End
+      </li>
+      */
